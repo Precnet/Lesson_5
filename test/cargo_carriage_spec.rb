4380fd2eebff098ce03ecc60d87480145fa496bd
Dmitry Davdov <dmfrodol@yandex.ru>
2019-11-13 20:44:04 +0300

    add tests for carriage manufacturer

--- test/cargo_carriage_spec.rb
+++ test/cargo_carriage_spec.rb
@@ -2,8 +2,14 @@ require 'rspec'
 require_relative '../cargo_carriage.rb'
 
 describe 'CargoCarriage' do
+  before(:all) do
+    @carriage = CargoCarriage.new
+  end
   it 'should create cargo carriage' do
-    carriage = CargoCarriage.new
-    expect(carriage.type).to eq('cargo')
+    expect(@carriage.type).to eq('cargo')
+  end
+  it 'should have manufacturer name' do
+    @carriage.set_manufacturer_name('Train inc.')
+    expect(@carriage.get_manufacturer_name).to eq('Train inc.')
   end
 end
--- test/passenger_carriage_spec.rb
+++ test/passenger_carriage_spec.rb
@@ -2,8 +2,14 @@ require 'rspec'
 require_relative '../passenger_carriage.rb'
 
 describe 'PassengerCarriage' do
+  before(:all) do
+    @carriage = PassengerCarriage.new
+  end
   it 'should create passenger carriage' do
-    carriage = PassengerCarriage.new
-    expect(carriage.type).to eq('passenger')
+    expect(@carriage.type).to eq('passenger')
+  end
+  it 'should have manufacturer name' do
+    @carriage.set_manufacturer_name('Train inc.')
+    expect(@carriage.get_manufacturer_name).to eq('Train inc.')
   end
 end
