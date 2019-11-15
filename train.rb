b85d4f951e451c2b632d41e601f8cc96bc9525dd
tester <dmfrodol@yandex.ru>
2019-11-15 05:26:14 -0800

    refactor robocop

--- train.rb
+++ train.rb
@@ -1,7 +1,7 @@
 require_relative 'manufacturer.rb'
 require_relative 'instance_counter.rb'
 
-TRAIN_TYPES = %w(passenger cargo)
+TRAIN_TYPES = %w[passenger cargo].freeze
 
 class Train
   include Manufacturer
@@ -11,7 +11,8 @@ class Train
               :number_of_carriages, :route
   @@trains = []
 
-  def initialize(train_type, number_of_carriages, train_number = generate_train_number(10))
+  def initialize(train_type, number_of_carriages,
+                 train_number = generate_train_number(10))
     @number = train_number
     @number_of_carriages = check_number_of_carriages(number_of_carriages)
     @type = check_train_type(train_type)
@@ -23,26 +24,19 @@ class Train
   end
 
   def self.find_train_by_number(number)
-    result = @@trains.select {|train| train.number == number}
+    result = @@trains.select { |train| train.number == number }
     result[0]
   end
 
-  def increase_speed_by(km)
-    @current_speed += km
-    if @current_speed > 120
-      @current_speed = 120
-      # puts "Train is at it`s maximum speed - 120km/h. It can`t safely increase
-      # it`s speed any more."
-    end
+  def increase_speed_by(number_of_km)
+    @current_speed += number_of_km
+    @current_speed = 120 if @current_speed > 120
     current_speed
   end
 
-  def decrease_speed_by(km)
-    @current_speed -= km
-    if @current_speed < 0
-      @current_speed = 0
-      # puts "Train had already stopped. It can`t decrease it`s speed any more."
-    end
+  def decrease_speed_by(number_of_km)
+    @current_speed -= number_of_km
+    @current_speed = 0 if @current_speed.negative?
     current_speed
   end
 
@@ -51,12 +45,16 @@ class Train
   end
 
   def add_carriage
-    raise RuntimeError, 'Can`t add new carriages while train is moving.' unless @current_speed == 0
+    moving_message = 'Can`t add new carriages while train is moving.'
+    raise moving_message if @current_speed.nonzero?
+
     @number_of_carriages += 1
   end
 
   def remove_carriage
-    raise RuntimeError, 'There are no carriages to remove.' unless @number_of_carriages > 0
+    no_carriages_message = 'There are no carriages to remove.'
+    raise no_carriages_message unless @number_of_carriages.positive?
+
     @number_of_carriages -= 1
   end
 
@@ -67,27 +65,33 @@ class Train
 
   def move_forward
     check_route
-    event_no_next_station = 'Train is already at it`s final station and can`t move further!'
-    raise event_no_next_station unless next_station_available?
+    next_message = 'Train is already at it`s final station and can`t move further!'
+    raise next_message unless next_station_available?
+
     @current_station = @route.stations[next_station_index]
   end
 
   def move_backward
     check_route
-    event_no_previous_station = 'Train is already at it`s first station and can`t move backward!'
-    raise event_no_previous_station unless previous_station_available?
+    previous_message = 'Train is already at it`s first station and can`t move backward!'
+    raise previous_message unless previous_station_available?
+
     @current_station = @route.stations[previous_station_index]
   end
 
   def get_previous_station
     check_route
-    raise RuntimeError, 'Can`t get previous station for first station!' unless previous_station_available?
+    no_station = 'Can`t get previous station for first station!'
+    raise no_station unless previous_station_available?
+
     @route.stations[previous_station_index]
   end
 
   def get_next_station
     check_route
-    raise RuntimeError, 'Can`t get next station for last station!' unless next_station_available?
+    no_station = 'Can`t get next station for last station!'
+    raise no_station unless next_station_available?
+
     @route.stations[next_station_index]
   end
 
@@ -96,21 +100,22 @@ class Train
   # should be private because there is no need to call it in descendants
   def generate_train_number(number_length)
     # nice little magick with converting int to str with Base36
-    rand(36 ** number_length).to_s(36)
+    rand(36**number_length).to_s(36)
   end
 
   # should be private because descendants are created without any carriages
-  def check_number_of_carriages(number_of_carriages)
-    event_wrong_number_carriages = "Number of carriages should be positive Integer. Got: #{number_of_carriages}"
-    raise ArgumentError, event_wrong_number_carriages unless
-        number_of_carriages.is_a?(Integer) && number_of_carriages >= 0
-    number_of_carriages
+  def check_number_of_carriages(number)
+    wrong_message = "Number of carriages should be positive Integer. Got: #{number}"
+    raise ArgumentError, wrong_message unless number.is_a?(Integer) && number >= 0
+
+    number
   end
 
   # should be private because descendants are created without train type selection
   def check_train_type(train_type)
-    event_wrong_train_type = "Wrong type of a train! Should be 'cargo' or 'passenger'. Got - '#{train_type}'"
-    raise ArgumentError, event_wrong_train_type  unless TRAIN_TYPES.include? train_type
+    wrong_message = "Should be 'cargo' or 'passenger'. Got - '#{train_type}'"
+    raise ArgumentError, wrong_message unless TRAIN_TYPES.include? train_type
+
     train_type
   end
 
