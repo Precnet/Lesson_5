1be3f30d63aacf982ce0c0b94b0bdf63308f7ba3
Dmitry Davdov <dmfrodol@yandex.ru>
2019-11-15 06:45:21 +0300

    carriages: move common code to parent class

--- carriage.rb
+++ carriage.rb
@@ -1,1 +1,14 @@
+class Carriage
+  include Manufacturer
 
+  NUMBER_LENGTH = 5
+  BASE_36 = 36
+  CARRIAGE_TYPE = ''
+
+  protected
+
+  # this is a method for creating default name for carriage it should not be used outside of object constructor
+  def generate_carriage_number(number_length)
+    CARRIAGE_TYPE + '_' + rand(BASE_36 ** number_length).to_s(BASE_36)
+  end
+end
--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -1,10 +1,7 @@
 require_relative 'manufacturer.rb'
+require_relative 'carriage.rb'
 
-class PassengerCarriage
-  include Manufacturer
-
-  NUMBER_LENGTH = 5
-  BASE_36 = 36
+class PassengerCarriage < Carriage
   CARRIAGE_TYPE = 'passenger'
 
   attr_reader :type, :number
@@ -12,12 +9,5 @@ class PassengerCarriage
   def initialize(carriage_number = generate_carriage_number(NUMBER_LENGTH))
     @type = CARRIAGE_TYPE
     @number = carriage_number
-  end
-
-  private
-
-  # this is a method for creating default name for carriage it should not be used outside of object constructor
-  def generate_carriage_number(number_length)
-    CARRIAGE_TYPE + '_' + rand(BASE_36 ** number_length).to_s(BASE_36)
   end
 end
