731273dba4a6e34ae9a93775cf5074005a10de8e
tester <dmfrodol@yandex.ru>
2019-11-15 04:43:53 -0800

    refactor: freeze

--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -2,7 +2,7 @@ require_relative 'manufacturer.rb'
 require_relative 'carriage.rb'
 
 class PassengerCarriage < Carriage
-  CARRIAGE_TYPE = 'passenger'
+  CARRIAGE_TYPE = 'passenger'.freeze
 
   def initialize(carriage_number = generate_carriage_number(NUMBER_LENGTH))
     super carriage_number
